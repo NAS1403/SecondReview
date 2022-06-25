5daf22e12bb1b0479a0a2d68aeb8520b0466a283
N Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-06-25 10:42:24 +0530

    [ABHI]Second review programs

---
+++ Array.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+declare -a Myarray
+value=0
+Myarray=(4 5 8 7 6)
+echo ${#Myarray[@]}
+read -p "Enter the number to be found in array: " num
+
+for ((i=0;i<${#Myarray[@]};i++))
+do
+	if ((${Myarray[i]}==$num))
+	then
+		value=${Myarray[i]}
+	fi
+done
+
+if ((value==0))
+then
+	echo "Number does not exist in the array"
+else
+	echo "Number exists in the array"
+fi
---
+++ reverse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+read -p "Enter the number to be reversed: " Num
+
+while (($Num!=0))
+do
+	n=$((Num%10))
+	sum=$((sum*10+n))
+	Num=$((Num/10))
+done
+
+echo "Number after reversing is" $sum
+
